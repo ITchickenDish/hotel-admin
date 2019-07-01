c4d3f73dc7190ebbbf7e8337daeba6b06d9df77f
ITchickenDish <491051587@qq.com>
2019-07-01 11:28:50 +0800

    数据库配置

--- src/gui/LoginGUI.java
+++ src/gui/LoginGUI.java
@@ -1,4 +1,4 @@
-package theDatabase.hotelAdmin.gui;
+package gui;
 
 import java.awt.*;
 import javax.swing.*;
--- src/gui/RegisteredGUI.java
+++ src/gui/RegisteredGUI.java
@@ -1,4 +1,4 @@
-package theDatabase.hotelAdmin.gui;
+package gui;
 
 import javax.swing.*;
 
